ee1f33d4c94fb94b69087e30a05672f46b0e2d1d
dev-chloe <chloekim.4969@gmail.com>
2021-08-23 21:57:37 +0900

    Ch34 DB 입력

--- test/controllers/admin/admin.ctrl.js
+++ test/controllers/admin/admin.ctrl.js
@@ -1,3 +1,5 @@
+const models = require('../../models');
+
 exports.get_products = (_, res) => {
   res.render('admin/products.html', {
       message: "hello"
@@ -9,6 +11,20 @@ exports.get_products_write = (_, res) => {
   res.render('admin/write.html');
 }
 
-exports.post_products_write = (req, res) => {s
-  res.send(req.body);
+exports.post_products_write = (req, res) => {
+  // res.send(req.body);
+
+  // 하나 하나 넣을 때
+  // models.Products.create({
+  //   name: req.body.name,
+  //   price: req.body.price,
+  //   description: req.body.description
+  // }).then(() => {
+  //   res.redirect('/admin/products');
+  // });
+
+  models.Products.create(req.body).then(() => {
+    res.redirect('/admin/products');
+  });
+  
 }
