a774e7bfda3d2843e1815fc3cd3557ef0f45a495
dev-chloe <chloekim.4969@gmail.com>
2021-08-29 19:16:25 +0900

    Ch42 async await

--- test/controllers/admin/admin.ctrl.js
+++ test/controllers/admin/admin.ctrl.js
@@ -1,71 +1,67 @@
 const models = require('../../models');
 
-exports.get_products = (_, res) => {
-  models.Products.findAll({
-
-  // }).then( (products) => {
-  //   res.render('admin/products.html', { products : products }) // 같은 이름은 아래처럼 한번만 써도 된다
-  // }) 
-  }).then( (products) => {
-    res.render('admin/products.html', { products })
-  })
+exports.get_products = async (_, res) => {
+  try {
+    const products = await models.Products.findAll();
+    res.render('admin/products.html', { products });
+  } catch(e) {
+    console.error(e)
+  }
 }
 
 exports.get_products_write = (_, res) => {
   res.render('admin/write.html');
 }
 
-exports.post_products_write = (req, res) => {
-  // res.send(req.body);
-
-  // 하나 하나 넣을 때
-  // models.Products.create({
-  //   name: req.body.name,
-  //   price: req.body.price,
-  //   description: req.body.description
-  // }).then(() => {
-  //   res.redirect('/admin/products');
-  // });
-
-  models.Products.create(req.body).then(() => {
+exports.post_products_write = async (req, res) => {
+  try{
+    await models.Products.create(req.body);
     res.redirect('/admin/products');
-  });
+  } catch(e) {
+    console.error(e)
+  }
 }
 
-exports.get_products_detail = (req, res) => {
-  // req.params.id
-  models.Products.findByPk(req.params.id).then( (product) => {
+exports.get_products_detail = async (req, res) => {
+  try{
+    const product = await models.Products.findByPk(req.params.id);
     res.render('admin/detail.html', { product });  
-    // console.log(product)
-    // res.send(product);
-  });
+  } catch(e) {
+    console.error(e)
+  }
 };
 
-exports.get_products_edit = (req, res) => {
-  models.Products.findByPk(req.params.id).then( (product) => {
+exports.get_products_edit = async (req, res) => {
+  try{
+    const product = await models.Products.findByPk(req.params.id);
     res.render('admin/write.html', { product });  
-  });
+  } catch(e) {
+    console.error(e)
+  }
 };
 
-exports.post_products_edit = (req, res) => {
-  models.Products.update({
-    // 데어터
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description,
-  },{
-    where: { id: req.params.id }
-  }).then(() => {
+exports.post_products_edit = async (req, res) => {
+  try{
+    await models.Products.update(
+      req.body,
+      {
+      where: { id: req.params.id }
+    })
     res.redirect('/admin/products/detail/' + req.params.id);
-  })
+  } catch(e) {
+    console.error(e)
+  }
 };
 
-exports.get_products_delete  = (req, res) => {
-  models.Products.destroy({
-    where: {
-      id: req.params.id
-    }
-  }).then( () => {
+exports.get_products_delete = async (req, res) => {
+  try{
+    await models.Products.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
     res.redirect('/admin/products')
-  })
+  } catch(e) {
+    console.error(e)
+  }
 };
