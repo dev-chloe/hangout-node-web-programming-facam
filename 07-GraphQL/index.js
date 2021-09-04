5b6ee5ed281208e7c0ced74421fc7cec5418435f
dev-chloe <chloekim.4969@gmail.com>
2021-09-04 22:24:04 +0900

    Ch49 데이터 읽기

--- 07-GraphQL/index.js
+++ 07-GraphQL/index.js
@@ -3,22 +3,38 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
+  type Product {
+    id: ID!
+    name: String
+    price: Int
+    description: String
+  }
   type Query {
-    hello: String,
-    nodejs: Int
+    getProduct( id: ID! ): Product
   }
 `);
 
+const product = [{
+  id: 1,
+  name: '제품일',
+  price: 5000,
+  description: '첫번째 제품입니다'
+},{
+  id: 2,
+  name: '제품이',
+  price: 10000,
+  description: '두번째 제품입니다'
+}]
+
 const root = {
-  hello : () => 'hellow world',
-  nodejs: () => 23
+  getProduct: ({ id }) => product.find( product => product.id === parseInt(id) )
 }
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: false
+  graphiql: true
 }));
 
 app.listen( 4000, () => {
