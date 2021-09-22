c204839dc60bbb43d0bf24f06fcf50161ae663f9
dev-chloe <chloekim.4969@gmail.com>
2021-09-22 19:32:19 +0900

    Ch65 Redis 캐싱

--- test/controllers/admin/admin.ctrl.js
+++ test/controllers/admin/admin.ctrl.js
@@ -1,8 +1,48 @@
 const models = require('../../models');
+const redis = require('redis');
+const redisClient = redis.createClient(); 
+const { promisify } = require("util");
+
+redisClient.on('error', function (err) {
+  console.log('Error ' + err);
+});
+
+// const getAsync = (key) => new Promise( (resolve , reject ) => {
+//   redisClient.get( key , ( err , data) => {
+//     if(err) reject(err);
+//     if(data){
+//       resolve(data);
+//     }else{
+//       resolve(null);
+//     }
+//   });
+// })
+
+const getAsync = promisify(redisClient.get).bind(redisClient);
+// getAsync.then(console.log).catch(console.error);
 
 exports.get_products = async (_, res) => {
   try {
+    let results = JSON.parse(await getAsync("products:all"));
+    
+    // let results = await getAsync("products:all");
+
+    // redisClient.get("products:all", (err, data) => {
+    //   if(data) {
+    //     results = data;
+    //   } else {
+    //     results = null;
+    //   }
+    // })
+
+    console.log(results)
+    
+
     const products = await models.Products.findAll();
+
+    if(!results) {
+      results = products;
+    }
     res.render('admin/products.html', { products });
   } catch(e) {
     console.error(e)
@@ -16,6 +56,10 @@ exports.get_products_write = (_, res) => {
 exports.post_products_write = async (req, res) => {
   try{
     await models.Products.create(req.body);
+    
+    const products = await models.Products.findAll();
+    redisClient.set( "products:all" , JSON.stringify(products))
+
     res.redirect('/admin/products');
   } catch(e) {
     console.error(e)
