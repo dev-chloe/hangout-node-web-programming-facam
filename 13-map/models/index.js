fd16bd8b0817eafa1a3721c2373918cf2ddc2de6
dev-chloe <chloekim.4969@gmail.com>
2021-10-04 22:50:39 +0900

    Ch74 지도 검색해서 위도 경도 DB 등록

--- 13-map/models/index.js
+++ 13-map/models/index.js
@@ -5,37 +5,51 @@ var dotenv = require('dotenv');
 
 dotenv.config(); //LOAD CONFIG
 
-const sequelize = new Sequelize( process.env.DATABASE,
-process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    timezone: '+09:00', //한국 시간 셋팅
-    operatorsAliases: Sequelize.Op,
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+
+const wkx = require('wkx')
+Sequelize.GEOMETRY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOMETRY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+
+const sequelize = new Sequelize(process.env.DATABASE,
+	process.env.DB_USER, process.env.DB_PASSWORD, {
+		host: process.env.DB_HOST,
+		dialect: 'mysql',
+		timezone: '+09:00', //한국 시간 셋팅
+		operatorsAliases: Sequelize.Op,
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000
+		}
+	});
 
 let db = [];
 
 fs.readdirSync(__dirname)
-    .filter(file => {
-        return file.indexOf('.js')&& file !== 'index.js'
-    })
-    .forEach(file => {
-        var model = sequelize.import(path.join(__dirname,
-            file));
-            db[model.name] = model;
-    });
+	.filter(file => {
+		return file.indexOf('.js') && file !== 'index.js'
+	})
+	.forEach(file => {
+		var model = sequelize.import(path.join(__dirname,
+			file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach(modelName => {
-    if("associate" in db[modelName]){
-        db[modelName].associate(db);
-    }
+	if ("associate" in db[modelName]) {
+		db[modelName].associate(db);
+	}
 });
-
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
