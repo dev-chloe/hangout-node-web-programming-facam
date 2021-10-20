971e2c836c44af3cd8ce559ccc157a55c991a683
dev-chloe <chloekim.4969@gmail.com>
2021-10-20 22:26:24 +0900

    Ch103 Arrow Functions 연습문제2

--- 14-Node/ch25-ArrowFunctions/test.js
+++ 14-Node/ch25-ArrowFunctions/test.js
@@ -4,8 +4,29 @@ function getDiscount(price, rate) {
   return price * rate
 }
 
-console.log(getDiscount(10000, 0.1))
+// console.log(getDiscount(10000, 0.1))
 
 // arrow func
 const getDiscountArrowFun = (price, rate) => price * rate
-console.log(getDiscountArrowFun(10000, 0.2))
+// console.log(getDiscountArrowFun(10000, 0.2))
+
+
+const doDiscount = function getDiscount(rate) {
+  return function (price) {
+    return rate * price
+  }
+}
+
+// arrow func
+const doDiscountArrowFun = rate => price => rate * price
+
+const getTenpercentOff = doDiscountArrowFun(0.1)
+
+// getTenpercentOff(price);
+
+console.log(getTenpercentOff(1000))
+/*
+f(x) = x + 1
+g(x) = x^2
+h(x) = f(g(x))
+*/
