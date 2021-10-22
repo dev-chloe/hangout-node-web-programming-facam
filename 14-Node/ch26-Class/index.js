d1e62f305b90812a1172b6e71cd51ae2dafbef5d
dev-chloe <chloekim.4969@gmail.com>
2021-10-22 22:27:22 +0900

    Ch106 Class 추가

--- 14-Node/ch26-Class/index.js
+++ 14-Node/ch26-Class/index.js
@@ -7,3 +7,19 @@ class cacheManager {
 }
 
 const CacheManager = new cacheManager()
+
+class Robot {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name}`)
+  }
+}
+
+const r = new Robot('hi');
+r.speak()
+// hi
+
+// constructor에서는 초기 값을 받을 수 있다.
