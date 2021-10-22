77cd52490d0c6c3c855f3e4dfdbeb8c3ab284f3e
dev-chloe <chloekim.4969@gmail.com>
2021-10-22 22:32:27 +0900

    Ch107 Class Extends 추가

--- 14-Node/ch26-Class/index.js
+++ 14-Node/ch26-Class/index.js
@@ -11,6 +11,7 @@ const CacheManager = new cacheManager()
 class Robot {
   constructor(name) {
     this.name = name;
+    // constructor에서는 초기 값을 받을 수 있다.
   }
 
   speak() {
@@ -18,8 +19,23 @@ class Robot {
   }
 }
 
+class Ai extends Robot {
+  constructor(name) {
+    super(name)
+    // super는 상위 클래스의 생성자를 호출하는 기능이다
+  }
+  walk() {
+    console.log(`walk: ${this.name}`)
+  }
+}
+
+
 const r = new Robot('hi');
-r.speak()
+r.speak();
 // hi
 
-// constructor에서는 초기 값을 받을 수 있다.
+const a = new Ai('hey');
+a.speak();
+// hey
+a.walk();
+//walk: hey
