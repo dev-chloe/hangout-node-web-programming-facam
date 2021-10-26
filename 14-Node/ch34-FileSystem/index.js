4014e069f4cc6ab7e1c519edb32cf4456cfa7c8e
dev-chloe <chloekim.4969@gmail.com>
2021-10-26 22:25:34 +0900

    Ch116 File System2

--- 14-Node/ch34-FileSystem/index.js
+++ 14-Node/ch34-FileSystem/index.js
@@ -1,20 +1,39 @@
 'use strict';
 
 const fs = require('fs');
+const { promisify } = require('util');
 
-fs.readFile('test.txt', 'utf-8', (err, data) => {
-  if(err) {
-    console.log(err)
-    return
-  }
-  console.log(data);
-})
+const read = promisify(fs.readFile);
+const write = promisify(fs.writeFile);
 
-const content = "hi this is chloe."
-fs.writeFile('test.txt', content, err => {
-  if(err) {
-    console.log(err)
-    return
+const writeAndRead = async (data = '') => {
+  try {
+    await write('test.txt', data);
+    // const content = await read('test.txt');
+    // return content;
+    return (await read('test.txt'))
+  } catch(e) {
+    console.error(e);
   }
-  console.log('success');
-})
+}
+
+writeAndRead('this is right');
+
+/*
+  fs.readFile('test.txt', 'utf-8', (err, data) => {
+    if(err) {
+      console.log(err)
+      return
+    }
+    console.log(data);
+  })
+
+  const content = "hi this is chloe."
+  fs.writeFile('test.txt', content, err => {
+    if(err) {
+      console.log(err)
+      return
+    }
+    console.log('success');
+  })
+*/
